04fda2ae78207462f47e9dfa4b6e2c801d0144bb
Justin Richard <richardj@MA8528-AppDev.local>
2019-03-31 21:35:08 -0500

    Assign2

---
+++ Toy2/CatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CatTableViewCell.swift
+//  Toy2
+//
+//  Created by Justin Richard on 3/31/19.
+//  Copyright © 2019 Justin Richard. All rights reserved.
+//
+
+import UIKit
+
+class CatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Toy2/Models/Cat.swift
@@ -0,0 +1,13 @@
+//
+//  Cat.swift
+//  Toy2
+//
+//  Created by Justin Richard on 3/31/19.
+//  Copyright © 2019 Justin Richard. All rights reserved.
+//
+
+import UIKit
+
+class Cat: NSObject {
+
+}
